ed28b78c550a0f4a168ee88bfbd86ef684205c78
Nadia Prasetyo <nadiakinsellap@gmail.com>
2025-07-14 11:41:06 +1200

    checking issues with debug

--- bin/antigen_analysis.py
+++ bin/antigen_analysis.py
@@ -219,6 +219,8 @@ def main():
 
     tool_paths = {}
 
+    print("DEBUG: fasta_files =", fasta_files)
+    print("DEBUG: checking tool paths=", args.tools)
     # Only resolve tool_root if required
     if any(tool in args.tools for tool in ["SIGNALP", "TARGETP"]):
         if args.tool_root == "none":
@@ -226,12 +228,15 @@ def main():
             sys.exit(1)
     
     tool_root = Path(args.tool_root).resolve()
+    print("DEBUG: tool_root =", tool_root)
     
     try:
         tool_paths = common.check_antigen_tools(args.tools, tool_root)
     except (FileNotFoundError, ImportError) as e:
         logging.error(f"❌ {e}")
         sys.exit(1)
+
+    print("DEBUG: tool_paths =", tool_paths)
 
     output_root = data_path / args.output_dir
     output_root.mkdir(parents=True, exist_ok=True)
