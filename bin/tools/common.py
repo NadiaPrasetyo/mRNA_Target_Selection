08e194a1e913437938d18d658b9527594db804a6
Nadia Prasetyo <nadiakinsellap@gmail.com>
2025-07-21 14:35:52 +1200

    correcting placement of prepare output dir

--- bin/tools/common.py
+++ bin/tools/common.py
@@ -373,7 +373,7 @@ def prepare_output_dirs(pathogen_path, output_subdir, selected_tools):
     for tool in selected_tools:
         (output_dir / tool.lower()).mkdir(parents=True, exist_ok=True)
 
-    return 
+    return output_dir
 
 
 def cleanup_temp(temp):
