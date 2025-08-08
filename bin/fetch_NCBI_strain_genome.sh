011a9401af1019761ac22b96c4cf218df97465e1
Nadia Prasetyo <nadiakinsellap@gmail.com>
2025-08-08 23:00:09 +1200

    correcting genome fetch to include length constrainst

--- bin/fetch_NCBI_strain_genome.sh
+++ bin/fetch_NCBI_strain_genome.sh
@@ -87,8 +87,24 @@ if [ "$RANDOM_MODE" = true ]; then
 
     echo "Fetching $RANDOM_NUM random complete genomes for pathogen: $PATHOGEN_NAME"
 
-    # Fetch random genome IDs
-    RANDOM_GENOME_IDS=$(esearch -db nucleotide -query "\"${PATHOGEN_NAME}\"[Organism] AND \"complete genome\"[All Fields]" | \
+    # get the reference genome for the pathogen
+    REFERENCE_GENOME=$(esearch -db nucleotide -query "\"${PATHOGEN_NAME}\"[Organism] AND \"complete genome\"[All Fields]" | \
+        efetch -format docsum | \
+        xtract -pattern DocumentSummary -element Caption | \
+        head -n 1) # Get the first result as the reference genome
+
+    if [ -z "$REFERENCE_GENOME" ]; then
+        echo "Error: No reference genome found for pathogen: $PATHOGEN_NAME"
+        exit 1
+    fi
+
+    # Extract the sequence length from the reference genome
+    REFERENCE_LENGTH=$(esearch -db nucleotide -query "\"${REFERENCE_GENOME}\"[Caption]" | \
+        efetch -format docsum | \
+        xtract -pattern DocumentSummary -element Slen)
+
+    # Fetch random genome IDs that have sequence length within the range of 500,000 bp above or under the reference genome of the pathogen
+    RANDOM_GENOME_IDS=$(esearch -db nucleotide -query "\"${PATHOGEN_NAME}\"[Organism] AND \"complete genome\"[All Fields] AND (\"$((REFERENCE_LENGTH - 500000))\"[SLEN] : \"$((REFERENCE_LENGTH + 500000))\"[SLEN])" | \
         efetch -format docsum | \
         xtract -pattern DocumentSummary -element Caption | \
         shuf -n "$RANDOM_NUM") # Get random IDs by shuffling the results and selecting the top N
