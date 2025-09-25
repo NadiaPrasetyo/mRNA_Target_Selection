4066b400e156f7fcf9348c09ad812cbc3b692681
Nadia Prasetyo <nadiakinsellap@gmail.com>
2025-09-25 20:16:42 +1200

    correcting alleles for mhci and mhcii

--- bin/tools/run_mhci.py
+++ bin/tools/run_mhci.py
@@ -34,10 +34,13 @@ from pathlib import Path
 import logging
 
 MHCI_EXTENDED = [
-    "HLA-A*01:01", "HLA-A*02:01", "HLA-A*02:03", "HLA-A*02:06", "HLA-A*03:01", "HLA-A*11:01", "HLA-A*23:01", "HLA-A*24:02",
-    "HLA-A*26:01", "HLA-A*30:01", "HLA-A*30:02", "HLA-A*31:01", "HLA-A*32:01", "HLA-A*33:01", "HLA-A*68:01", "HLA-A*68:02",
-    "HLA-B*07:02", "HLA-B*08:01", "HLA-B*15:01", "HLA-B*35:01", "HLA-B*40:01", "HLA-B*44:02", "HLA-B*44:03", "HLA-B*51:01",
-    "HLA-B*53:01", "HLA-B*57:01", "HLA-B*58:01"
+    "HLA-A01:01", "HLA-A02:01", "HLA-A02:03", "HLA-A02:06",
+    "HLA-A03:01", "HLA-A11:01", "HLA-A23:01", "HLA-A24:02",
+    "HLA-A26:01", "HLA-A30:01", "HLA-A30:02", "HLA-A31:01",
+    "HLA-A32:01", "HLA-A33:01", "HLA-A68:01", "HLA-A68:02",
+    "HLA-B07:02", "HLA-B08:01", "HLA-B15:01", "HLA-B35:01",
+    "HLA-B40:01", "HLA-B44:02", "HLA-B44:03", "HLA-B51:01",
+    "HLA-B53:01", "HLA-B57:01", "HLA-B58:01"
 ]
 
 def run(fasta_file, tool_path, output_dir):
